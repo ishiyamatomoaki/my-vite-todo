e3748aaeaf7eb29f961b680795960be181c5f890
ishiyamatomoaki <t-ishiyama@aile-283.com>
2023-05-30 08:37:07 +0900

    その他機能

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,26 @@
 <script setup>
 import TheHeader from './components/TheHeader.vue';
 import TheFooter from './components/TheFooter.vue';
-import MainTodo from './components/MainTodo.vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const goBlog = () => {
+  router.push('/blog');
+};
 </script>
 
 <template>
   <div class="wrap">
-    <div><TheHeader /></div>
-    <main class="main"><MainTodo /></main>
-    <div><TheFooter /></div>
+    <TheHeader />
+    <div id="nav">
+      <router-link to="/">Home</router-link>
+      | <router-link to="/about">About</router-link> |
+      <span @click="goBlog">Blog</span>
+      | <router-link to="/blog/1">Blog1</router-link> |
+      <router-link to="/blog/2">Blog2</router-link>
+    </div>
+    <main class="main"><router-view /></main>
+    <TheFooter />
   </div>
 </template>
 
@@ -18,7 +30,7 @@ import MainTodo from './components/MainTodo.vue';
 .wrap {
   display: flex;
   flex-direction: column;
-  /* align-items: center;これがあるとヘッダーの背景色の幅が狭くなる */
+  align-items: center;
   min-height: 91vh;
   width: 370px;
   margin: 0 auto;
---
+++ src/pages/Blog.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { ref, watch } from 'vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+const id = ref(route.params.id);
+console.log('watch外:', id);
+
+watch(route, () => {
+  id.value = route.params.id;
+  console.log('watch内:', id.value);
+});
+
+const posts = ref([]);
+const fetchDate = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  posts.value = await response.json();
+};
+fetchDate();
+</script>
+
+<template>
+  <ul>
+    <li v-for="post in posts" :key="post.id">
+      {{ post.id }};
+      <router-link :to="`/blog/${post.id}`">{{ post.title }}</router-link>
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+ul {
+  margin-top: 12px;
+}
+
+li {
+  margin-bottom: 8px;
+  border: 1px solid #ccc;
+  padding: 8px;
+}
+
+li:hover {
+  background-color: #eee;
+}
+</style>
